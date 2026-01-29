96e461d5ba305de8b09f7d2e1ee6bf5eafac69ea
gui1416 <guilhermerm2005@gmail.com>
2026-01-29 14:32:18 -0300

    fix: fuzziness

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -40,7 +40,7 @@ export async function POST(req: NextRequest) {
           multi_match: {
             query,
             fields: ["conteudo", "arquivo^2"],
-            fuzziness: "AUTO",
+            fuzziness: "2",
           },
         },
         highlight: {
